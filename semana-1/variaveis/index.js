029adf1ba648bd5c933ee38a792524e43e2b64ef
Tainá Soares <tainasoares10eler@gmail.com>
2021-08-22 21:33:08 -0300

    Atualizei a pasta de variáveis

--- semana-1/variaveis/index.js
+++ semana-1/variaveis/index.js
@@ -43,8 +43,8 @@ alert(`Voce recebe ${t/p} por hora`)
 O programa calcula quando uma pessoa recebe
 por hora, dividindo o valor recebido do dia
 por horas trabalhadas naquele dia.
-Seria interessante trocar p = horasTrabalhadasDia
-e t = valorTrabalhoDia
+Seria interessante trocar p = horasTrabalhadasPorDia
+e t = valorTrabalhoPorDia
 */
 //********************************* 
 
@@ -64,15 +64,16 @@ para estas variaveis
 */
 
 nome = prompt("Qual é o seu nome?")
-idade = prompt("Qual é a sua idade?")
-console.log(typeof nome)
-console.log(typeof idade)
+idade = Number(prompt("Qual é a sua idade?"))
+console.log(typeof (nome))
+console.log(typeof (idade))
 
 /*(e-f) Agora é pedido para que o usuário
 atribua valores a estas duas variáveis,
 então como é utilizado o prompt, este 
 retornará string string, mesmo se eu tiver
-tratando de números
+tratando de números. Por isso é colocado o
+Number na variável idade.
 */
 
 console.log("Olá", nome, "você tem", idade, "anos.")
@@ -81,14 +82,22 @@ console.log("Olá", nome, "você tem", idade, "anos.")
 
 // Exercícío 2
 
-const roupa = "Você está usando roupa azul?"
-console.log(roupa, true)
+let roupa = prompt("Você está de roupa azul?Sim ou Não?")
+let comida = prompt("Você gosta de pizza?Sim ou Não?")
+let cor = prompt("Você gosta de rosa?Sim ou Não?")
 
-const clima = "Está muito frio hoje?"
-console.log(clima, true)
+//a.
+const roupaAzul = roupa
+const gostaDePizza = comida
+const gostaDeRosa = cor
+//b.
+const fraseRoupaAzul = `Você está de roupa azul? - ${roupaAzul}`
+const fraseGostaDePizza = `Você gosta de pizza? - ${gostaDePizza}`
+const fraseGostaDeRosa = `Você gosta de rosa? - ${gostaDeRosa}`
 
-const corFavorita = "Sua cor favorita é rosa?"
-console.log(corFavorita, false)
+console.log(fraseRoupaAzul)
+console.log(fraseGostaDePizza)
+console.log(fraseGostaDeRosa)
 
 //********************************* 
 
@@ -105,3 +114,17 @@ b = c
 
 console.log("O novo valor de a é", a) // O novo valor de a é 25
 console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+// Desafio
+
+const numero1 = Number(prompt("Insira um número"))
+const numero2 = Number(prompt("Insira um número"))
+
+const soma = numero1 + numero2
+const multiplicar = numero1 * numero2
+
+const fraseSoma = `O primeiro número somado ao segundo número resulta em: ${soma}`
+const fraseMultiplicacao = `O primeiro número multiplicado pelo segundo número resulta em: ${multiplicar}`
+
+console.log(fraseSoma)
+console.log(fraseMultiplicacao)
