9bf41c16e08891dd5d41bf946c8ddf3c2887bb61
Tainá Soares <tainasoares10eler@gmail.com>
2022-01-21 22:21:25 -0300

    Atualizando

--- semana18/aula52/src/index.ts
+++ semana18/aula52/src/index.ts
@@ -1,14 +1,41 @@
 import app from "./app"
 import { Request, Response } from "express"
 import { Transaction, UserAccount } from "./types"
+// import { Transaction, UserAccount } from "./types"
 
 app.get("/", (req: Request, res: Response)=>{
     res.send("Hello World!")
 })
 
 
-const myAccount = new UserAccount("111-111-111-11", "alguem", 20)
-console.log(myAccount)
-const myTransfer = new Transaction("2021-12-06", 50, "pagando")
-myAccount.setTransactions(myTransfer)
-console.log(myAccount.getTransactions())
+// const myAccount = new UserAccount("111-111-111-11", "alguem", 20)
+// console.log(myAccount)
+// const myTransfer = new Transaction("2021-12-06", 50, "pagando")
+// myAccount.setTransactions(myTransfer)
+// console.log(myAccount.getTransactions())
+
+//QUESTÃO 1
+//Instânciando a classe UserAccount
+
+// const userAccount = new UserAccount("000-000-000-00", "João", 25)
+// console.log(userAccount) //1b)é chamado uma vez
+// userAccount.setTransactions(transaction)
+
+//QUESTÃO 2
+
+// const transaction = new Transaction("Pagamento efetuado",20,"20/02/2021")
+// console.log(transaction)
+const userAccount = new UserAccount("000-000-000-00", "João", 25)
+// console.log(userAccount) //1b)é chamado uma vez
+// userAccount.setTransactions(transaction)
+
+
+
+
+// const myUser = new UserAccount('222.111.333-21','Jessica', 25)
+//    console.log(myUser.getCpf())
+//    console.log(myUser.getName())
+//    console.log(myUser.getAge())
+//    myUser.setTransactions(transaction)
+//    console.log(myUser.getTransaction())
+//    console.log("")
--- semana18/aula52/src/types.ts
+++ semana18/aula52/src/types.ts
@@ -1,17 +1,109 @@
-export class Transaction {
+// export class Transaction {
+//     private description: string;
+//     private value: number;
+//     private date: string;
+
+//     constructor(
+//         description: string,
+//         value: number,
+//         date: string,
+//      ) {
+//         this.description = description;
+//         this.value = value;
+//         this.date = date;
+//     }
+
+//     public getDescription():string {
+//         return this.description
+//     }
+
+//     public getValue():number {
+//         return this.value
+//     }
+
+//     public getDate():string {
+//         return this.date
+//     }      
+    
+// }
+
+
+// export class UserAccount {
+//     private cpf: string;
+//     private name: string;
+//     private age: number;
+//     private balance: number = 0;
+//     private transactions: Transaction[] = [];
+  
+//     constructor(
+//        cpf: string,
+//        name: string,
+//        age: number,
+//     ) {
+//        console.log("Chamando o construtor da classe UserAccount")
+//        this.cpf = cpf;
+//        this.name = name;
+//        this.age = age;
+//     }
+
+//     public getCpf():string {
+//         return this.cpf
+//     }
+
+//     public getName():string {
+//         return this.name
+//     }
+
+//     public getAge():number {
+//         return this.age
+//     }
+    
+//     public getBalance():number {
+//         return this.balance
+//     } 
+
+//     public getTransactions():Transaction[] {
+//         return this.transactions
+//     } 
+
+//     setTransactions(newTransaction: Transaction):void{
+//         console.log(this.transactions)
+//         this.transactions.push(newTransaction)
+//     }
+
+// }
+
+
+
+// export class Bank {
+//     private accounts: UserAccount[];
+  
+//     constructor(accounts: UserAccount[]) {
+//       this.accounts = accounts;
+//     }
+
+   
+  
+// }
+
+// const myAccount = new UserAccount("112536522", "alguem", 20)
+// const myTransfer = new Transaction("2021-12-06", 50, "pagango")
+// myAccount.setTransactions(myTransfer)
+// console.log(myAccount.getTransactions())
+
+
+export class Transaction  {
     private description: string;
     private value: number;
     private date: string;
 
-    constructor(
-        description: string,
-        value: number,
-        date: string,
-     ) {
-        this.description = description;
-        this.value = value;
-        this.date = date;
+    constructor(description:string, value:number, date:string){
+        this.description = description
+        this.value = value
+        this.date = date
     }
+
+    //Acesso desses valores privados
 
     public getDescription():string {
         return this.description
@@ -23,10 +115,8 @@ export class Transaction {
 
     public getDate():string {
         return this.date
-    }      
-    
+    }
 }
-
 
 export class UserAccount {
     private cpf: string;
@@ -57,36 +147,17 @@ export class UserAccount {
     public getAge():number {
         return this.age
     }
-    
-    public getBalance():number {
-        return this.balance
-    } 
-
-    public getTransactions():Transaction[] {
-        return this.transactions
-    } 
-
-    setTransactions(newTransaction: Transaction):void{
-        console.log(this.transactions)
-        this.transactions.push(newTransaction)
+    public setAge(newAge:number):void{
+        this.age = newAge
     }
 
-}
-
-
-
-export class Bank {
-    private accounts: UserAccount[];
-  
-    constructor(accounts: UserAccount[]) {
-      this.accounts = accounts;
+    setTransactions(newTransaction: Transaction) : void {
+        this.transactions.push(newTransaction) 
     }
 
-   
+    getTransaction(): Transaction[]{
+    return this.transactions
+    }
   
 }
 
-// const myAccount = new UserAccount("112536522", "alguem", 20)
-// const myTransfer = new Transaction("2021-12-06", 50, "pagango")
-// myAccount.setTransactions(myTransfer)
-// console.log(myAccount.getTransactions())
